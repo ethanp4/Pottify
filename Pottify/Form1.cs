a88e4d83f44ba83210cd0ec01daa4052fe5738af
ethanp4 <157554972+ethanp4@users.noreply.github.com>
2024-11-19 13:13:36 -0700

    not working

--- Pottify/Form1.cs
+++ Pottify/Form1.cs
@@ -1,27 +1,39 @@
 using System.Diagnostics;
 using System.Reflection;
 using System.IO;
+using Id3;
 
 namespace Pottify {
     public partial class Form1 : Form {
         public Form1() {
             InitializeComponent();
-            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            Debug.WriteLine(path);
+            //https://github.com/JeevanJames/Id3
             var songsPath = "..\\..\\..\\Songs";
+            //DirectoryInfo d = new DirectoryInfo(songsPath); 
+            string[] files = Directory.GetFiles(songsPath, "*.mp3");
+
+            foreach (var file in files) {
+                setNothing(file);
+            }
 
 
-            DirectoryInfo d = new DirectoryInfo(songsPath); 
-
-            FileInfo[] files = d.GetFiles("*.mp3");
-            string str = "";
-
-            Debug.WriteLine(files);
+            void setNothing(string mp3FilePath) {
+                using (var mp3 = new Mp3(mp3FilePath, Mp3Permissions.ReadWrite)) {
+                    Id3Tag tag = mp3.GetTag(Id3TagFamily.Version2X); //get the existing tag
+                    tag = new();
+                    tag.Year.Value = 2000;
+                    tag.Title = "Unknown";
+                    mp3.WriteTag(tag, WriteConflictAction.Replace); //rewrite that tag to the file
+                }
+            }
 
             songsList.Items.AddRange(files);
-            foreach (FileInfo file in files) {
-                Debug.WriteLine(file.FullName);
-            }
+
+            var songs = new AutoCompleteStringCollection();
+            songs.AddRange(files);
+            textSearch.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textSearch.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            textSearch.AutoCompleteCustomSource = songs;
         }
     }
 }
