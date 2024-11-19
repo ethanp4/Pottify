c57080ca505af18a44cc93db36d36764f6929ca9
ethanp4 <157554972+ethanp4@users.noreply.github.com>
2024-11-19 12:47:10 -0700

    Add project files.

---
+++ Pottify/Form1.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace Pottify {
+    partial class Form1 {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            songsList = new ListBox();
+            SuspendLayout();
+            // 
+            // songsList
+            // 
+            songsList.FormattingEnabled = true;
+            songsList.Location = new Point(64, 46);
+            songsList.Name = "songsList";
+            songsList.Size = new Size(638, 344);
+            songsList.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(songsList);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListBox songsList;
+    }
+}
