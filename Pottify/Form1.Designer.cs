7a5c4ec6a89c5b79a643bc3dc46e551801dac870
ethanp4 <157554972+ethanp4@users.noreply.github.com>
2024-11-19 21:07:13 -0700

    start song class and metadata loading

--- Pottify/Form1.Designer.cs
+++ Pottify/Form1.Designer.cs
@@ -23,8 +23,14 @@
         ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            ListViewItem listViewItem1 = new ListViewItem("dsadsad");
             songsList = new ListBox();
             textSearch = new TextBox();
+            songsListView = new ListView();
+            Title = new ColumnHeader();
+            Artist = new ColumnHeader();
+            Length = new ColumnHeader();
+            Genre = new ColumnHeader();
             SuspendLayout();
             // 
             // songsList
@@ -32,7 +38,7 @@
             songsList.FormattingEnabled = true;
             songsList.Location = new Point(64, 46);
             songsList.Name = "songsList";
-            songsList.Size = new Size(638, 344);
+            songsList.Size = new Size(178, 344);
             songsList.TabIndex = 0;
             // 
             // textSearch
@@ -44,11 +50,39 @@
             textSearch.TabIndex = 1;
             textSearch.TextChanged += searchChanged;
             // 
+            // songsListView
+            // 
+            songsListView.Columns.AddRange(new ColumnHeader[] { Title, Artist, Length, Genre });
+            songsListView.Items.AddRange(new ListViewItem[] { listViewItem1 });
+            songsListView.Location = new Point(248, 46);
+            songsListView.Name = "songsListView";
+            songsListView.Size = new Size(454, 344);
+            songsListView.TabIndex = 2;
+            songsListView.UseCompatibleStateImageBehavior = false;
+            songsListView.View = View.Details;
+            // 
+            // Title
+            // 
+            Title.Text = "Title";
+            // 
+            // Artist
+            // 
+            Artist.Text = "Artist";
+            // 
+            // Length
+            // 
+            Length.Text = "Length";
+            // 
+            // Genre
+            // 
+            Genre.Text = "Genre";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(songsListView);
             Controls.Add(textSearch);
             Controls.Add(songsList);
             Name = "Form1";
@@ -61,5 +95,10 @@
 
         private ListBox songsList;
         private TextBox textSearch;
+        private ListView songsListView;
+        private ColumnHeader Title;
+        private ColumnHeader Artist;
+        private ColumnHeader Length;
+        private ColumnHeader Genre;
     }
 }
