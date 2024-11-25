2bb5939454d79141204efd90f8100ae34561b87f
Ethan <157554972+ethanp4@users.noreply.github.com>
2024-11-25 15:15:45 -0700

    properly load songs

--- Pottify/Form1.cs
+++ Pottify/Form1.cs
@@ -4,43 +4,21 @@ using System.IO;
 
 namespace Pottify {
     public partial class Form1 : Form {
-        string[] files;
         public Form1() {
             InitializeComponent();
             //https://github.com/mono/taglib-sharp
+
             var songsPath = "..\\..\\..\\Songs";
-            //DirectoryInfo d = new DirectoryInfo(songsPath); 
-            files = Directory.GetFiles(songsPath, "*.mp3");
-
-            //foreach (var file in files) {
-            //    var tfile = TagLib.File.Create(file);
-            //    string title = tfile.Tag.Title;
-            //    TimeSpan duration = tfile.Properties.Duration;
-            //    Debug.WriteLine("Title: {0}, duration: {1}", title, duration);
-
-            //    // change title in the file
-            //    tfile.Tag.Title = "my new title";
-            //    tfile.Save();
-            //}
-
-            foreach (var file in files) {
-                var tfile = TagLib.File.Create(file);
-                var title = tfile.Tag.Title;
-                var duration = tfile.Properties.Duration;
-                var author = tfile.Tag.Performers;
-                var genre = tfile.Tag.Genres;
-                var s = new Song(title, author, Convert.ToInt32(duration.TotalSeconds), genre, file);
-                
-                songsListView.Items.Add();
-            }
+            // songsPath = @"C:\Users\Ethan\Music\";
+            Song.initSongList(songsPath);
 
             var listItem = new ListViewItem();
             songsListView.Items.Add(listItem);
 
-            songsList.Items.AddRange(files);
+            // songsList.Items.AddRange(files);
 
             var songs = new AutoCompleteStringCollection();
-            songs.AddRange(files);
+            // songs.AddRange(files);
             textSearch.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
             textSearch.AutoCompleteSource = AutoCompleteSource.CustomSource;
             textSearch.AutoCompleteCustomSource = songs;
@@ -50,11 +28,11 @@ namespace Pottify {
             List<string> res = new List<string>();
             var query = textSearch.Text;
 
-            foreach (var item in files) {
-                if (item.Contains(query)) {
-                    res.Add(item);
-                }
-            }
+            // foreach (var item in files) {
+            //     if (item.Contains(query)) {
+            //         res.Add(item);
+            //     }
+            // }
             songsList.Items.Clear();
             songsList.Items.AddRange(res.ToArray());
         }
