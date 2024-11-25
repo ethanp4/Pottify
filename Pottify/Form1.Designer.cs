b686c1f923e09975cc5a42171cc759f65dc7f896
Ethan <157554972+ethanp4@users.noreply.github.com>
2024-11-25 16:41:59 -0700

    search and double click event

--- Pottify/Form1.Designer.cs
+++ Pottify/Form1.Designer.cs
@@ -22,69 +22,40 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent() {
-            ListViewItem listViewItem1 = new ListViewItem("dsadsad");
-            songsList = new ListBox();
+        private void InitializeComponent()
+        {
             textSearch = new TextBox();
             songsListView = new ListView();
-            Title = new ColumnHeader();
-            Artist = new ColumnHeader();
-            Length = new ColumnHeader();
-            Genre = new ColumnHeader();
             SuspendLayout();
-            // 
-            // songsList
-            // 
-            songsList.FormattingEnabled = true;
-            songsList.Location = new Point(64, 46);
-            songsList.Name = "songsList";
-            songsList.Size = new Size(178, 344);
-            songsList.TabIndex = 0;
             // 
             // textSearch
             // 
-            textSearch.Location = new Point(64, 13);
+            textSearch.Location = new Point(56, 10);
+            textSearch.Margin = new Padding(3, 2, 3, 2);
             textSearch.Name = "textSearch";
-            textSearch.PlaceholderText = "Search for a song, album, or artist";
-            textSearch.Size = new Size(638, 27);
+            textSearch.PlaceholderText = "Search for a song";
+            textSearch.Size = new Size(901, 23);
             textSearch.TabIndex = 1;
             textSearch.TextChanged += searchChanged;
             // 
             // songsListView
             // 
-            songsListView.Columns.AddRange(new ColumnHeader[] { Title, Artist, Length, Genre });
-            songsListView.Items.AddRange(new ListViewItem[] { listViewItem1 });
-            songsListView.Location = new Point(248, 46);
+            songsListView.Location = new Point(56, 34);
+            songsListView.Margin = new Padding(3, 2, 3, 2);
             songsListView.Name = "songsListView";
-            songsListView.Size = new Size(454, 344);
+            songsListView.Size = new Size(901, 550);
             songsListView.TabIndex = 2;
             songsListView.UseCompatibleStateImageBehavior = false;
             songsListView.View = View.Details;
             // 
-            // Title
-            // 
-            Title.Text = "Title";
-            // 
-            // Artist
-            // 
-            Artist.Text = "Artist";
-            // 
-            // Length
-            // 
-            Length.Text = "Length";
-            // 
-            // Genre
-            // 
-            Genre.Text = "Genre";
-            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1071, 670);
             Controls.Add(songsListView);
             Controls.Add(textSearch);
-            Controls.Add(songsList);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
@@ -92,13 +63,7 @@
         }
 
         #endregion
-
-        private ListBox songsList;
         private TextBox textSearch;
         private ListView songsListView;
-        private ColumnHeader Title;
-        private ColumnHeader Artist;
-        private ColumnHeader Length;
-        private ColumnHeader Genre;
     }
 }
--- Pottify/Form1.cs
+++ Pottify/Form1.cs
@@ -4,37 +4,72 @@ using System.IO;
 
 namespace Pottify {
     public partial class Form1 : Form {
+        private List<ListViewItem> fullList = new();
         public Form1() {
             InitializeComponent();
             //https://github.com/mono/taglib-sharp
 
             var songsPath = "..\\..\\..\\Songs";
             // songsPath = @"C:\Users\Ethan\Music\";
+
+            //load songs and set listview columns
             Song.initSongList(songsPath);
-
-            var listItem = new ListViewItem();
-            songsListView.Items.Add(listItem);
-
-            // songsList.Items.AddRange(files);
-
+            songsListView.MouseDoubleClick += songDoubleClick;
+            songsListView.MultiSelect = false;
+            songsListView.FullRowSelect = true;
+            songsListView.Columns.Add("Title", 200);
+            songsListView.Columns.Add("Artist", 200);
+            songsListView.Columns.Add("Album", 200);
+            songsListView.Columns.Add("Track", 70);
+            songsListView.Columns.Add("Year", 100);
+            foreach (var s in Song.songsList) {
+                var listItem = new ListViewItem();
+                listItem.Text = s.title;
+                listItem.SubItems.Add(s.artist[0]);
+                listItem.SubItems.Add(s.album);
+                listItem.SubItems.Add($"{s.trackNumber} of {s.trackCount}");
+                listItem.SubItems.Add(s.year == 0 ? "Not set" : s.year.ToString());
+                listItem.Tag = s; //get data from here when clicked or something
+                songsListView.Items.Add(listItem);
+            }
+            foreach(var i in songsListView.Items) {
+                fullList.Add((ListViewItem)i);
+            }
+            // fullList.AddRange((IEnumerable<ListViewItem>)songsListView.Items); //need to create a copy for filtering
+            //set autocomplete source
             var songs = new AutoCompleteStringCollection();
-            // songs.AddRange(files);
-            textSearch.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            foreach(var title in Song.songsList.Select(s => s.title)) {
+                songs.Add(title);
+            }
+            textSearch.AutoCompleteMode = AutoCompleteMode.Suggest;
             textSearch.AutoCompleteSource = AutoCompleteSource.CustomSource;
             textSearch.AutoCompleteCustomSource = songs;
         }
 
-        private void searchChanged(object sender, EventArgs e) {
-            List<string> res = new List<string>();
-            var query = textSearch.Text;
+        private void songDoubleClick(object sender, EventArgs e) {
+            Song selectedSong = (Song)songsListView.SelectedItems[0].Tag;
+            Debug.WriteLine("song was double clicked " + selectedSong);
+        }
 
-            // foreach (var item in files) {
-            //     if (item.Contains(query)) {
-            //         res.Add(item);
-            //     }
-            // }
-            songsList.Items.Clear();
-            songsList.Items.AddRange(res.ToArray());
+        //searching for now
+        private void searchChanged(object sender, EventArgs e) {
+            // var query = sender.Text.ToLower();
+            var query = textSearch.Text.ToLower();
+            if (query == "") {
+                songsListView.Items.Clear();
+                songsListView.Items.AddRange(fullList.ToArray());
+                return;
+            }
+            // var items = songsListView.Items;
+            var items = fullList;
+            var res = new List<ListViewItem>();
+            foreach(ListViewItem item in items) {
+                if (item.Text.ToLower().Contains(query)) {
+                    res.Add(item);
+                }
+            }
+            songsListView.Items.Clear();
+            songsListView.Items.AddRange(res.ToArray());
         }
     }
 }
