d673d1082c7092256000b7b7c7fc63a1af2da0b2
Ethan <157554972+ethanp4@users.noreply.github.com>
2024-11-30 15:25:46 -0700

    createSongListViewItem() and bug fix and start info form

---
+++ Pottify/SongInfoEditForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pottify
+{
+    public partial class SongInfoEditForm : Form
+    {
+        public SongInfoEditForm(Song s)
+        {
+            InitializeComponent();
+
+        }
+    }
+}
