f65bb44f424a119adfcf5da526c520ece0528227
Ethan <157554972+ethanp4@users.noreply.github.com>
2024-12-01 21:28:23 -0700

    change form size

--- Pottify/SongInfoEditForm.Designer.cs
+++ Pottify/SongInfoEditForm.Designer.cs
@@ -260,7 +260,7 @@
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(718, 302);
             Controls.Add(label9);
             Controls.Add(label8);
             Controls.Add(label7);
